e10432e0be1817f02a91df9fb321869e1ea461d6
Prasen Gope <prasen.gope@hotmail.com>
2019-06-29 16:39:31 -0400

    Updates

---
+++ precache-manifest.15bcfd9bd7bca997c2d4d396cdd7e555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c23e77ed44d59fe9eaffe13755e03a5",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "54f9eedb88e3ae7bfa6e",
+    "url": "/clickygame/static/css/main.85e573d5.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "54f9eedb88e3ae7bfa6e",
+    "url": "/clickygame/static/js/main.c4947564.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
